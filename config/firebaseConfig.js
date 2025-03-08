14580191367debf54bb9fee2b0e4ba644d4f1f79
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-03-08 18:52:12 +0530

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,5 +1,18 @@
+// const admin = require("firebase-admin");
+// const serviceAccount = require("../firebaseConfig.json");
+// admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+// const db = admin.firestore();
+// module.exports = { db };
+
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebaseConfig.json");
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+// Parse Firebase credentials from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 const db = admin.firestore();
+
 module.exports = { db };
