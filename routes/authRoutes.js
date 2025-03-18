eb79828476e2e6efc4456cb8122cbee420bce7e3
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-03-18 18:32:18 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -69,7 +69,7 @@ router.get("/callback", async (req, res) => {
 
     // Redirect to frontend dashboard
     // res.redirect("http://localhost:8080/dashboard"); // 🔹 Change to your frontend URL
-    res.json({ message: "Login successful", user: req.user });  // ✅ Send a response instead
+    // res.json({ message: "Login successful", user: req.user });  // ✅ Send a response instead
 
   } catch (error) {
     console.error("❌ Auth Callback Error:", error);
